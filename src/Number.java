ba9b2efdfc86b02cb042b0ce62bcb9b8a1ad1151
Ruslan Fursa <fursa_1986@mail.ru>
2023-01-09 18:00:58 +0300

    change range logic

--- src/Number.java
+++ src/Number.java
@@ -6,7 +6,7 @@ public class Number {
         int randomNum = (int)(Math.random() * 99 + 1);
         Scanner in = new Scanner(System.in);
         System.out.println("Hello friend!!! Input any number from 1 to 100 included");
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 7; i++) {
             int num;
             try {
                 num = in.nextInt();
@@ -16,26 +16,20 @@ public class Number {
                 i--;
                 continue;
             }
+            if (num < 1 || num > 100) {
+                System.out.println("Number should be between 1 and 100 included");
+                i--;
+                continue;
+            }
+
             if (num == randomNum) {
                 System.out.println("You win");
                 in.close();
                 return;
-            } else if (num < 1) {
-                System.out.println("Number should be > 0");
-//                in.next();
-//                i--;
-//                continue;
-            }
-            else if (num < randomNum) {
-                System.out.println("Your number is less than our. Try again");
-            }  else if (num > 100){
-                System.out.println("Number should be < 101");
-//                in.next();
-//                i--;
-//                continue;
-            }
 
-            else {
+            } else if (num < randomNum) {
+                System.out.println("Your number is less than our. Try again");
+            } else {
                 System.out.println("Your number is more than our. Try again");
             }
 
